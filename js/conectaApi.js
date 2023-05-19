c725d516fc9dc875ad2652a12e2dddb4e6698fd6
Victor Candido <victorjrcandido@gmail.com>
2023-05-19 20:26:44 -0300

    funcao criaVideo

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,6 +4,24 @@ async function listaVideos() {
   return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, imagem, url) {
+  const conexao = await fetch("http://localhost:3000/videos", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      imagem: imagem,
+      url: url
+    })
+  });
+  const conexaoConvertida = await conexao.json();
+
+  return conexaoConvertida;
+};
+
+
 export const conectaApi = {
-  listaVideos
+  listaVideos,
+  criaVideo,
 };
--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,17 +2,23 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard() {
+function constroiCard(titulo, descricao, imagem, url) {
   const video = document.createElement("li");
   video.className = "video__item";
   video.innerHTML = `
-  <iframe width="100%" height="72%" src="https://www.youtube.com/embed/HohH6hymUgM" title="Don&#39;t try to build this on Pantheon..." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <iframe width="100%" height="72%" src="${url}" title="${titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   <div class="descricao-video">
-      <img src="./img/logo.png" alt="logo canal alura">
-      <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-      <p>236 mil visualizações</p>
+      <img src="${imagem}" alt="logo canal alura">
+      <h3>${titulo}</h3>
+      <p>${descricao}</p>
   </div>`
   return video
 };
 
 
+async function listaVideos() {
+  const listaApi = await conectaApi.listaVideos();
+  listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.imagem, elemento.url)));
+}
+
+listaVideos();
