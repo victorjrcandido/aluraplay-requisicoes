98fd3617fd9cf5aaf9daf8b697b3a2507a0a424f
Victor Candido <victorjrcandido@gmail.com>
2023-07-16 18:51:56 -0300

    3.1

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -1,27 +1,10 @@
 async function listaVideos() {
-  const conexao = await fetch("http://localhost:3000/videos");
-  const conexaoConvertida = await conexao.json();
-  return conexaoConvertida;
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, imagem, url) {
-  const conexao = await fetch("http://localhost:3000/videos", {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({
-      titulo: titulo,
-      descricao: `${descricao} mil visualizações`,
-      imagem: imagem,
-      url: url
-    })
-  });
-  const conexaoConvertida = await conexao.json();
-
-  return conexaoConvertida;
-};
-
-
 export const conectaApi = {
-  listaVideos,
-  criaVideo,
-};
+    listaVideos
+}
--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,23 +2,26 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, imagem, url) {
-  const video = document.createElement("li");
-  video.className = "video__item";
-  video.innerHTML = `
-  <iframe width="100%" height="72%" src="${url}" title="${titulo}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-  <div class="descricao-video">
-      <img src="${imagem}" alt="logo canal alura">
-      <h3>${titulo}</h3>
-      <p>${descricao}</p>
-  </div>`
-  return video
-};
+function constroiCard(titulo, descricao, url, imagem) {
+    const video = document.createElement("li");
+    video.className = "videos__item";
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descricao-video">
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
+</div>`
 
+    return video;
+}
 
 async function listaVideos() {
-  const listaApi = await conectaApi.listaVideos();
-  listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.imagem, elemento.url)));
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
 }
 
 listaVideos();
